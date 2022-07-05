ce16878e742e94119b58dbf34851b0414d4fade9
mabus <m.buscaroli@outlook.com>
2022-07-05 14:22:08 +0100

    first commit, basic setup

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from werkzeug import exceptions
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def home():
+    return jsonify({'message': 'Hello to Cards!'}), 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
